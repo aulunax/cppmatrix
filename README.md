caa5e7471c901a6bf8dc868f83a3541ba4ffde58
aulunax <kamilony@gmail.com>
2024-04-20 20:04:06 +0200

    improved README

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
-### Prerequisites
-- cpyhton3.6 or higher (with python-dev)
+# PyMatrix
+## Prerequisites
+- cpyhton3.6 or higher (also python-dev and mypy are required) 
 - g++ with c++17 support
-- mypy for stubgen
-- cmake
-### Instruction
-# Linux:
-1. Navigate to the project folder
-2. mkdir build
-3. cd build
-4. cmake ..
-5. cmake --build .
-6. cd ..
-7. ./main.py
+- CMake
+## Instruction
+### Linux:
+1. Make sure all prerequisites are installed and updated
+2. Navigate to the project folder
+3. Create and enter build directory in the project folder
+```sh
+mkdir build
+cd build
+```
+4. Run CMake on project folder
+```sh
+cmake ..
+```
+5. Build the project in build directory using CMake
+```sh
+cmake --build .
+```
+6. Run the main.py
+```sh
+cd ..
+./main.py
+```
+### Windows:
+1. idk, good luck
--- main.py
+++ main.py
@@ -5,27 +5,30 @@ from python.gauss_seidel import gauss_seidel_iter_method
 from python.common import create_matrix
 from math import sin
 
-N = 948 # 948
-a1, a2, a3 = 7, -1, -1
 
-A = create_matrix(N,a1,a2,a3)
+for num in [100, 200, 500, 1000, 1500]:
+    print("\nMatrix size: " + str(num) + "\n")
+    N = num # 948
+    a1, a2, a3 = 7, -1, -1
 
-b = Matrix(N,1,1)
-for i in range(N):
-    b[(i,0)] = sin((i+1)*4)
+    A = create_matrix(N,a1,a2,a3)
 
-x, err_norms, iterations, time_taken = jacobi_iter_method(A, b, 100)
-print("Jacobi method:")
-print("iterations: " + str(iterations))
-print("time_taken: " + str(time_taken))
-print("err_norm: " + str(err_norms))
+    b = Matrix(N,1,1)
+    for i in range(N):
+        b[(i,0)] = sin((i+1)*4)
 
-x, err_norms, iterations, time_taken = gauss_seidel_iter_method(A, b, 100)
-print("Gauss-Seidel method:")
-print("iterations: " + str(iterations))
-print("time_taken: " + str(time_taken))
-print("err_norm: " + str(err_norms))
+    x, err_norms, iterations, time_taken = jacobi_iter_method(A, b, 100)
+    print("Jacobi method:")
+    print("iterations: " + str(iterations))
+    print("time_taken: " + str(time_taken))
+    print("err_norm: " + str(err_norms))
 
-x, time_taken = direct_method(A, b)
-print("Direct method:")
-print("time_taken: " + str(time_taken))
+    x, err_norms, iterations, time_taken = gauss_seidel_iter_method(A, b, 100)
+    print("Gauss-Seidel method:")
+    print("iterations: " + str(iterations))
+    print("time_taken: " + str(time_taken))
+    print("err_norm: " + str(err_norms))
+
+    x, time_taken = direct_method(A, b)
+    print("Direct method:")
+    print("time_taken: " + str(time_taken))
